4e967bec36d2172a4e21867c191cf9d7102f923b
masamune@example.com <ec2-user@ip-172-31-10-154.ap-northeast-1.compute.internal>
2023-05-06 21:55:47 +0000

    x

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,5 +2,16 @@
 
 @section('contents')
 <h1>掲示板</h1><br>
+<div class="all">
+    <div class="one">
+        <h2>1</h2>
+    </div>
+    <div class="two">
+        <h2>2</h2>
+    </div>
+    <div class="three">
+        <h2>3</h2>
+    </div>
+</div>
 <a href="/logout">ログアウト</a>
 @endsection
